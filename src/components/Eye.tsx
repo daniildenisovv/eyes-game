809455eacb7c18cd2d732b0ec87a9b43411e118c
archyCuber <denisov.v.daniil@gmail.com>
2025-03-29 23:00:15 +0300

    update pupil animation

--- src/components/Eye.tsx
+++ src/components/Eye.tsx
@@ -4,48 +4,76 @@ import { useMotionValue, useMotionValueEvent } from 'motion/react';
 import { Point } from '../types.ts';
 import { computePupilPositionTowardTarget } from '../utils/compute-utils';
 import { animate } from 'motion';
-import { useClickStore } from '../store';
+import { useClickStore, useConeStore } from '../store';
 
-export const Eye = () => {
+type Props = {
+  angle: number | null;
+  pupilCenterPoint: Point;
+  onPupilCenterPointChange: (point: Point) => void;
+};
+
+const Pupal = ({ angle, pupilCenterPoint, onPupilCenterPointChange }: Props) => {
   const { currentX, currentY } = useClickStore();
   const x = useMotionValue(CX);
   const y = useMotionValue(CY);
-  const [pupilCenterPoint, setPupilCenterPoint] = useState<Point>({ x: CX, y: CY });
-  const isFirstAnimation = useRef(true);
+  const animationAngle = useMotionValue(angle);
+  const isFirstAnimated = useRef(false);
 
-  const updatePupil = () => {
+  const updatePupilByPoints = () => {
     const px = x.get();
     const py = y.get();
-    console.log('DD points', x.get(), y.get());
-    setPupilCenterPoint(
-      isFirstAnimation.current ? { x: px, y: py } : computePupilPositionTowardTarget(px, py),
-    );
+    onPupilCenterPointChange({ x: px, y: py });
   };
 
-  useMotionValueEvent(x, 'change', updatePupil);
-  useMotionValueEvent(y, 'change', updatePupil);
+  const updatePupilByAngle = () => {
+    onPupilCenterPointChange(computePupilPositionTowardTarget(animationAngle.get()));
+  };
+
+  useMotionValueEvent(x, 'change', updatePupilByPoints);
+  useMotionValueEvent(y, 'change', updatePupilByPoints);
+  useMotionValueEvent(animationAngle, 'change', updatePupilByAngle);
 
   useEffect(() => {
-    if (currentX == null || currentY == null) return;
-
-    const { x: newX, y: newY } = isFirstAnimation.current
-      ? computePupilPositionTowardTarget(currentX, currentY)
-      : { x: currentX, y: currentY };
-
+    if (currentX == null || currentY == null || isFirstAnimated.current) return;
+    const initialAngle = Math.atan2(currentY - CY, currentX - CX);
+    const { x: newX, y: newY } = computePupilPositionTowardTarget(initialAngle);
     animate(x, newX, { duration: 1, ease: 'easeInOut' });
+
     animate(y, newY, {
       duration: 1,
       ease: 'easeInOut',
       onComplete: () => {
-        isFirstAnimation.current = false;
+        isFirstAnimated.current = true;
       },
     });
-  }, [currentX, currentY]);
+  }, [currentY, currentY]);
+
+  useEffect(() => {
+    if (angle === null) {
+      return;
+    }
+    animate(animationAngle, angle, { duration: 1, ease: 'easeInOut' });
+  }, [angle]);
 
   return (
     <>
-      <circle cx={CX} cy={CY} r={CIRCLE_RADIUS} stroke="white" strokeWidth={2} fill="black" />
       <circle r={PUPIL_RADIUS} fill="white" cx={pupilCenterPoint.x} cy={pupilCenterPoint.y} />
+    </>
+  );
+};
+
+export const Eye = () => {
+  const { angle } = useConeStore();
+  const [pupilCenterPoint, setPupilCenterPoint] = useState<Point>({ x: CX, y: CY });
+
+  return (
+    <>
+      <circle cx={CX} cy={CY} r={CIRCLE_RADIUS} stroke="white" strokeWidth={2} fill="black" />;
+      <Pupal
+        angle={angle}
+        pupilCenterPoint={pupilCenterPoint}
+        onPupilCenterPointChange={setPupilCenterPoint}
+      />
     </>
   );
 };
